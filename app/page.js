0e8e92a4da0f2d2eb44cd8812e4c82be3299d31b
Elena <elenaharan@gmail.com>
2024-08-12 08:39:05 +0100

    Use Next.js Link component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>The Wild oasis!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise!</h1>
+      <Link href="/cabins">Learn about luxury cabins</Link>
+    </div>
+  );
 }
