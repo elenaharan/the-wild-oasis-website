2e56df386490b39ba12bdfa36996635d699abcf9
Elena <elenaharan@gmail.com>
2024-08-13 07:55:05 +0100

    add metadata to app layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,16 @@
 import Navigation from "./components/Navigation";
 
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navigation />
         <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
       </body>
     </html>
   );
