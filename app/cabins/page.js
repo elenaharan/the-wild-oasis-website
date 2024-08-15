bbafc8ea49556186ede1a96da61329eace127cef
Elena <elenaharan@gmail.com>
2024-08-15 08:08:36 +0100

    add loader

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,7 +3,6 @@ import Counter from "../components/Counter";
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-  console.log(data);
 
   return (
     <div>
