aca9ea47e0db92c531993f233032665c22effa71
Elena <elenaharan@gmail.com>
2024-08-21 16:32:01 +0100

    Add nested routes and pages

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,4 +1,4 @@
-import ReservationCard from "./ReservationCard";
+import ReservationCard from "../../_components/ReservationCard";
 
 export default function Page() {
   // CHANGE
@@ -13,14 +13,20 @@ export default function Page() {
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
-          <a className="underline text-accent-500" href="/cabins">
+          <a
+            className="underline text-accent-500"
+            href="/cabins"
+          >
             luxury cabins &rarr;
           </a>
         </p>
       ) : (
         <ul className="space-y-6">
           {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
+            <ReservationCard
+              booking={booking}
+              key={booking.id}
+            />
           ))}
         </ul>
       )}
