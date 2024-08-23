fcaf89df229dd70a52de59d4a28f284ca046753d
Elena <elenaharan@gmail.com>
2024-08-23 12:21:04 +0100

    create and export supabase client

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from ("@supabase/supabase-js");
 
-const supabase = createClient(process.env.SUPABASE_URL)
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
