44d5a70bd1b6375257cb52c1a750cacd6eb46613
Elena <elenaharan@gmail.com>
2024-09-03 08:13:08 +0100

    Add date picker and reservation form components

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -74,6 +74,7 @@ export default async function Page({ params }) {
         <h2 className="text-5xl font-semibold text-center">
           Reserve {name} today. Pay on arrival.
         </h2>
+        <div></div>
       </div>
     </div>
   );
