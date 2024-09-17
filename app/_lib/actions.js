8e0be17b4060ccb7d20ce37ae605ffa940494c66
Elena <elenaharan@gmail.com>
2024-09-17 07:44:14 +0100

    Revalidate cache manually

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -27,7 +28,7 @@ export async function updateGuest(formData) {
 
   if (error) throw new Error("Guest could not be updated");
 
-  return data;
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
