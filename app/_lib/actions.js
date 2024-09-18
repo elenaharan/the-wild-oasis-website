38224e7ae47164e09d60fcc4bd2ffbf40034b289
Elena <elenaharan@gmail.com>
2024-09-18 07:10:03 +0100

    delete reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -6,7 +6,6 @@ import { supabase } from "./supabase";
 
 export async function updateGuest(formData) {
   const session = await auth();
-
   if (!session) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
@@ -29,6 +28,20 @@ export async function updateGuest(formData) {
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
