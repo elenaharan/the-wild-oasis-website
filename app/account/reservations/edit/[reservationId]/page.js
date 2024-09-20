be2865312442944dfd7df9d5d0fef50bb7557a62
Elena <elenaharan@gmail.com>
2024-09-20 12:23:58 +0100

    Implement new route

--- app/account/reservations/edit/[reservationId]/page.js
+++ app/account/reservations/edit/[reservationId]/page.js
@@ -18,11 +18,17 @@ export default function Page() {
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
           >
-            <option value="" key="">
+            <option
+              value=""
+              key=""
+            >
               Select number of guests...
             </option>
             {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
-              <option value={x} key={x}>
+              <option
+                value={x}
+                key={x}
+              >
                 {x} {x === 1 ? "guest" : "guests"}
               </option>
             ))}
