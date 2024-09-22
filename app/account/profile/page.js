dd387d1120bcfa5178331d2994ba1450839b1dc8
Elena <elenaharan@gmail.com>
2024-09-22 10:09:17 +0100

    delete log

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -10,7 +10,6 @@ export const metadata = {
 export default async function Page() {
   const session = await auth();
   const guest = await getGuest(session.user.email);
-  console.log("guest here:", guest);
 
   return (
     <div>
